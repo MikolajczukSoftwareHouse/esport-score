960131ad649349d115a5465178b78af9c680a83a
Bartlomiej Mikolajczuk <bartlomiej.mikolajczuk@outlook.com>
2024-02-27 13:39:21 +0100

    mobile app

---
+++ apps/esport-score-mobile/src/app/App.spec.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { render } from '@testing-library/react-native';
+
+import App from './App';
+
+test('renders correctly', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('heading')).toHaveTextContent(
+    'Welcome ExportScoreMobile 👋'
+  );
+});
