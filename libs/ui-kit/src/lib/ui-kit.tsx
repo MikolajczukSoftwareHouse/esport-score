c00d68f4781163c442dfb8372424657134455c76
Bartlomiej Mikolajczuk <bartlomiej.mikolajczuk@outlook.com>
2024-03-02 14:30:48 +0100

    added new lib

---
+++ libs/ui-kit/src/lib/ui-kit.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface UiKitProps {}
+
+export function UiKit(props: UiKitProps) {
+  return (
+    <div>
+      <h1>Welcome to UiKit!</h1>
+    </div>
+  );
+}
+
+export default UiKit;
